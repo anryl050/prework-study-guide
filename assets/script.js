f11231abf678e923cd8e176e9d1bd0b45efa3a3c
anryl050 <118693401+anryl050@users.noreply.github.com>
2022-11-27 15:27:19 -0800

    adding changes to the JS

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
     for(var x = 0; x < topics.length; x++) {
@@ -7,17 +8,18 @@ function listTopics() {
 }
 
 function selectTopic() {
-    if (topic === 'HTML') {
-        console.log("Let's study HTML!");
-    } else if (topic === 'CSS') {
-        console.log("Let's study CSS!");
-    } else if (topic === 'Git') {
-        console.log("Let's study Git!");
-    } else if (topic === 'JavaScript') {
-        console.log("Let's study JavaScript!");
+    if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
     } else {
-        console.log('Please try again!');
-    } 
-}
+      console.log('Please try again!');
+    }
+  }
 
 listTopics()
+selectTopic()
